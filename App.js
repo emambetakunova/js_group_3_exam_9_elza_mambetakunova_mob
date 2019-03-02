2c915ee05d1341250bb3351836f50224d53c3997
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-03-02 18:25:01 +0600

    add store

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {createStore, applyMiddleware} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import {Provider} from 'react-redux';
+import reducer from './src/store/reducers/contactReducer'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+import Home from "./src/containers/Home/Home";
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <View style={styles.container}>
+                    <Home/>
+                </View>
+            </Provider>
+        );
+    }
+}
