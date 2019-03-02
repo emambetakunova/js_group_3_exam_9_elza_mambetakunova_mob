570aa83023172b74c8d7557fe57a5f88a55eae18
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-03-02 18:26:33 +0600

    add pages

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import {StyleSheet, Image, View, Text, TouchableOpacity, Modal, ScrollView} from 'react-native';
+import {connect} from "react-redux";
+
+import ContactList from "../../components/ContactList/ContactList";
+import {closeModal, fetchContacts, openModal} from "../../store/actions/contactAction";
+
+const styles = StyleSheet.create({
+    contactWrap: {
+        flex: 2,
+        backgroundColor: '#fff',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        borderWidth: 1,
+        borderColor: 'black',
+        marginVertical: 10,
+        paddingHorizontal: 25,
+        paddingVertical: 10
+
+    },
+    contactMenu: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        marginVertical: 25
+    },
+    button: {
+        borderWidth: 1,
+        borderColor: 'black',
+        padding: 25,
+        backgroundColor: '#ad1283',
+        marginVertical: 25
+
+    },
+});
+
+
+class Home extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchContacts();
+    }
+
+    render() {
+        console.log(this.props.oneContact);
+        let contacts = null;
+        if (this.props.contacts) {
+            contacts = this.props.contacts.map((contact, id) => (
+                <TouchableOpacity key={contact.id} style={styles.contactWrap} onPress={() => this.openModal(id)}>
+                    <Image style={{width: 60, height: 60, marginHorizontal: 10}} source={{uri: contact.image}}/>
+                    <Text style={{fontSize: 20}}>{contact.contactName}</Text>
+                </TouchableOpacity>
+            ))
+        }
+        return (
+            <ScrollView>
+                <View style={styles.contactMenu}>
+                    <Text style={{fontWeight: 'bold', textAlign: 'center', fontSize: 30}}>Contacts</Text>
+                    {contacts}
+                    <Modal visible={this.props.showModal}
+                           onRequestClose={() => {
+                               console.log('Modal closed');
+                           }}>
+                        <ContactList/>
+                        <TouchableOpacity style={styles.button} onPress={this.props.closeModal}>
+                            <Text style={{fontSize: 22}}>Close modal</Text>
+                        </TouchableOpacity>
+                    </Modal>
+                </View>
+            </ScrollView>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        contacts: state.contacts,
+        showModal: state.showModal
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchContacts: () => dispatch(fetchContacts()),
+        openModal: (id) => dispatch(openModal(id)),
+        closeModal: () => dispatch(closeModal())
+    };
+
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
